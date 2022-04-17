07fe13405107d7b2896d002267056d7fc4477371
FabioFreire-ff <fabiosbfreire@gmail.com>
2022-04-17 03:50:26 -0300

    Adicionado implementação do cenário criar um novo trabalhador.

---
+++ features/step_definitions/trabalhador_steps.rb
@@ -0,0 +1,16 @@
+Given('Eu estou na pagina de cadastrar novo trabalhador') do
+  visit '/trabalhadors/new'
+end
+
+When('Eu crio um trabalhador com nomeCompleto {string}, dataNasc {string}, cpf {string}, email {string}') do |string, string2, string3, string4|
+  fill_in 'trabalhador[nomeCompleto]',:with => string
+  fill_in 'trabalhador[dataNasc]',:with => string2
+  fill_in 'trabalhador[cpf]',:with => string3
+  fill_in 'trabalhador[email]',:with => string4
+  click_button 'Create Trabalhador'
+  expect(page).to have_current_path(trabalhadors_path + '/' + Trabalhador.last.id.to_s)
+end
+
+Then('Eu vejo uma mensagem que o paciente foi criado com sucesso') do
+  expect(page).to have_content('Trabalhador was successfully created.')
+end
