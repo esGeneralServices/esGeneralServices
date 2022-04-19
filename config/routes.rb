9ce09262bd8ed7d1efb7539eefd9755b48f54a70
Torres <gustavo.storres@ufrpe.br>
2022-04-19 16:21:22 -0300

    mudanças rotas clientes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :clientes, controllers: {
-    sessions: 'clientes/sessions'
+    sessions: 'clientes/sessions',
+    registrations: 'clientes/registrations'
   }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
