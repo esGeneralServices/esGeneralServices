4f6ea0b00f4c25cd0a695c41e15b59906ea8680b
Marcelino Barros <marcelino05052000@gmail.com>
2022-04-19 16:19:32 -0300

    Mudanças nos views e model de admin para poder funcioanr o campo de nome

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,5 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nome, presence: true
 end
