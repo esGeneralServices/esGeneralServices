5157874078b4f83a3b42992bcf86e0062ab961bc
Torres <gustavo.storres@ufrpe.br>
2022-04-19 16:13:28 -0300

    Validate cpf e nome cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,6 +1,18 @@
+require 'cpf_cnpj'
+
 class Cliente < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nome, presence: true, length: {in: 3..40}
+  validate :cpf_valida
+
+
+  def cpf_valida
+    if cpf.present? && !CPF.valid?(cpf)
+      errors.add(:cpf, "Formato: XXX.XXX.XXX-XX")
+    end
+  end
 end
