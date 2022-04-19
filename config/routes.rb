56092f4f1fe1c3073fa1f48139535d5721b25607
FabioFreire-ff <fabiosbfreire@gmail.com>
2022-04-19 19:36:39 -0300

    Gerando devise controller trabalhador.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :trabalhadors
+  devise_for :trabalhadors, controllers: {
+    sessions: 'trabalhadors/sessions',
+    registrations: 'trabalhadors/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
