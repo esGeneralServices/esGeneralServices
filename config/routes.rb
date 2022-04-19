2c6083f43083d3bbe0497822f43fb48a068fab3c
Torres <gustavo.storres@ufrpe.br>
2022-04-19 15:32:37 -0300

    Generate controllers Cliente

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :clientes
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
