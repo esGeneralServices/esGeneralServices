64fef5251c21046de183f6a61070d3e9c2e018d4
FabioFreire-ff <fabiosbfreire@gmail.com>
2022-04-19 21:00:13 -0300

    validate nome,cpf e profissao de trabalhador.

--- app/models/trabalhador.rb
+++ app/models/trabalhador.rb
@@ -3,4 +3,14 @@ class Trabalhador < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nome, presence: true, length: {in: 3..40}
+  validate :cpf_valida
+  validates :profissao, presence: true, length: {in: 3..40}
+
+  def cpf_valida
+    if cpf.present? && !CPF.valid?(cpf)
+      errors.add(:cpf, "Formato: XXX.XXX.XXX-XX")
+    end
+  end
 end
