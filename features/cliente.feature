88ce1bc249b4d178d495492223e117bb7049c00c
jurasmelo <jurandir.melo@ufrpe.br>
2022-04-20 00:03:23 -0300

    Criado o Scenario remover cliente

--- features/cliente.feature
+++ features/cliente.feature
@@ -7,3 +7,9 @@ Feature:
     Given Estou na pagina de cadastrar cliente
     When eu preencho os dados nome 'Jurandir' cpf '007.587.974-35' email 'jsmelo1@gmail.com' password '123456' confirma password '123456'
     Then vejo que o meu cadastro foi efetuado
+
+  Scenario: remover cliente
+    Given Estou na pagina de cliente
+    And o cliente de nome 'Jurandir' cpf '007.587.974-35' email 'jsmelo1@gmail.com' password '123456' confirma password '123456' existe
+    When eu clico para remover o cliente de nome 'Jurandir'
+    Then vejo que o cliente foi corretamente removido
--- features/step_definitions/cliente_steps.rb
+++ features/step_definitions/cliente_steps.rb
@@ -14,3 +14,27 @@ end
 Then('vejo que o meu cadastro foi efetuado') do
   expect(page).to have_content('Welcome! You have signed up successfully.')
 end
+
+Given('Estou na pagina de cliente') do
+  visit 'clientes/sign_up'
+end
+
+Given('o cliente de nome {string} cpf {string} email {string} password {string} confirma password {string} existe') do |nome, cpf, email, pass, pass_conf|
+  fill_in 'cliente[nome]', :with => nome
+  fill_in 'cliente[cpf]', :with => cpf
+  fill_in 'cliente[email]', :with => email
+  fill_in 'cliente[password]', :with => pass
+  fill_in 'cliente[password_confirmation]', :with => pass_conf
+  click_button 'Sign up'
+end
+
+When('eu clico para remover o cliente de nome {string}') do |nome|
+  expect(page).to have_content(nome)
+  expect(page).to have_current_path('/')
+  click_link 'Minha conta'
+  click_button 'Cancel my account'
+end
+
+Then('vejo que o cliente foi corretamente removido') do
+  expect(page).to have_content('Bem vindo ao sistema!')
+end
