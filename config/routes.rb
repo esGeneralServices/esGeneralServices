6ade5057e5e658b85f045fb7dccdb88844d27f5d
FabioFreire-ff <fabiosbfreire@gmail.com>
2022-04-19 21:20:09 -0300

    Adicionando sign_out na rota trabalhador.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     sessions: 'trabalhadors/sessions',
     registrations: 'trabalhadors/registrations'
   }
+
+  devise_scope :trabalhador do
+    get '/trabalhadors/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
