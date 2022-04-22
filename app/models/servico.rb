f0e7f38849ab4ad1ed0b754ad1350c7d874b7a6c
jurasmelo <jurandir.melo@ufrpe.br>
2022-04-21 23:44:54 -0300

    Criação das Validations no Model Servico

--- app/models/servico.rb
+++ app/models/servico.rb
@@ -1,2 +1,10 @@
 class Servico < ApplicationRecord
+
+  validates :nome, presence: true, length: {in: 3..20}, format: {with: /\A[a-zA-z ]+\z/, message: "Utilize penas letras"}
+  validates :tipo, presence: true, length: {in: 3..20}, format: {with: /\A[a-zA-z ]+\z/, message: "Utilize apenas letras"}
+  validates :horaInicio, presence: true, time: true, message: "Utilize a hora no formato XX:XXam ou XX:XXpm"
+  validates :horaFinal, presence: true, time: true, message: "Utilize a hora no formato XX:XXam ou XX:XXpm"
+  validates :descricao, presence: true, length: {in: 3..150}, format: {with: /\A[a-zA-z0-9]+\z/, message: "Utilieze apenas letras e números"}
+  validates :valor, presence: true, length: {in: 4..20}, format: {with: /\A[0-9]-\d{2}\z/, message: "Utilize o valor no formato X.XX"}
+
 end
