11060917120ccb3cb43cd95b2760103a495fdde2
Marcelino Barros <marcelino05052000@gmail.com>
2022-04-30 01:02:33 -0300

    Testes de model de admin

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'admin sem nome' do
+    admin = Admin.new(nome: '', email: 'admin@admin.com', password:'123456', password_confirmation:'123456')
+    assert_not admin.save
+  end
+  test 'admin valido' do
+    admin = Admin.new(nome: 'Teste', email: 'admin@admin.com', password:'123456', password_confirmation:'123456')
+    assert admin.save
+  end
+
+  test 'admin sem email ' do
+    admin = Admin.new(nome: 'Teste', email: '', password:'123456', password_confirmation:'123456')
+    assert_not admin.save
+  end
 end
