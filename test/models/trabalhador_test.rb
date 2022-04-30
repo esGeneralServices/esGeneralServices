a7bc8f6ebf26c8aa8a41d8d0e325dfeae24d9fba
Marcelino Barros <marcelino05052000@gmail.com>
2022-04-30 01:08:51 -0300

    Testes de model de trabalhador

--- test/models/trabalhador_test.rb
+++ test/models/trabalhador_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class TrabalhadorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'trabalhador sem nome' do
+    trabalhador = Trabalhador.new(nome: '', cpf: '70369935454', profissao: 'Pedreiro', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
+    assert_not trabalhador.save
+  end
+  test 'trabalhador valido' do
+    trabalhador = Trabalhador.new(nome: 'Teste', cpf: '70369935454', profissao: 'Pedreiro', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
+    assert trabalhador.save
+  end
+  test 'trabalhador sem profissao' do
+    trabalhador = Trabalhador.new(nome: 'Teste', cpf: '70369935454', profissao: '', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
+    assert_not trabalhador.save
+  end
 end
