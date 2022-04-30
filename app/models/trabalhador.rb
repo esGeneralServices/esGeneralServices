5221683d8d3fa5691ea2b774195aa661a9fce8b3
Torres <gustavo.storres@ufrpe.br>
2022-04-30 16:40:02 -0300

    Adição entidade Serviço

--- app/models/trabalhador.rb
+++ app/models/trabalhador.rb
@@ -1,4 +1,7 @@
 class Trabalhador < ApplicationRecord
+
+  has_many :servicos, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
