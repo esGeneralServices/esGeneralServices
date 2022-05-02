2eda8a692ec2551e426acdb0991189e7d4305026
Marcelino Barros <marcelino05052000@gmail.com>
2022-05-01 23:13:24 -0300

    Mudança no horario dos testes para um horário no futuro mais distante

--- test/models/servico_test.rb
+++ test/models/servico_test.rb
@@ -5,13 +5,13 @@ class ServicoTest < ActiveSupport::TestCase
     trabalhador = Trabalhador.new(nome: 'Teste', cpf: '70369935454', profissao: 'Pedreiro', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
     assert trabalhador.save
     servico = Servico.new(nome: 'Jardinagem', descricao: 'Realizo a limpeza de quintais', valor: '150.00', horarioInicio: '2000-01-01 07:30:00 UTC',
-                          horarioTermino: '2000-01-01 08:30:00 UTC', data: '2022-05-01', trabalhador_id: trabalhador.id)
+                          horarioTermino: '2000-01-01 08:30:00 UTC', data: '2022-12-30', trabalhador_id: trabalhador.id)
     assert servico.save
   end
 
   test 'servico sem trabalhador' do
     servico = Servico.new(nome: 'Jardinagem', descricao: 'Realizo a limpeza de quintais', valor: '150.00', horarioInicio: '2000-01-01 07:30:00 UTC',
-                          horarioTermino: '2000-01-01 08:30:00 UTC', data: '2022-05-01')
+                          horarioTermino: '2000-01-01 08:30:00 UTC', data: '2022-12-30')
     assert_not servico.save
   end
 
@@ -19,7 +19,7 @@ class ServicoTest < ActiveSupport::TestCase
     trabalhador = Trabalhador.new(nome: 'Teste', cpf: '70369935454', profissao: 'Pedreiro', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
     assert trabalhador.save
     servico = Servico.new(nome: 'Jardinagem', descricao: '', valor: '150.00', horarioInicio: '2000-01-01 07:30:00 UTC',
-                          horarioTermino: '2000-01-01 08:30:00 UTC', data: '2022-05-01', trabalhador_id: trabalhador.id)
+                          horarioTermino: '2000-01-01 08:30:00 UTC', data: '2022-12-30', trabalhador_id: trabalhador.id)
     assert_not servico.save
   end
 
