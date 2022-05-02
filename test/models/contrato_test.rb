6b7b06c407a9ffa46114487076b1011f804993cd
Marcelino Barros <marcelino05052000@gmail.com>
2022-05-02 04:48:06 -0300

    Resolução de problemas nos testes de model de contrato derivados da adição de endereço ao cliente

--- test/models/contrato_test.rb
+++ test/models/contrato_test.rb
@@ -11,7 +11,8 @@ class ContratoTest < ActiveSupport::TestCase
     servico = Servico.new(nome: 'Jardinagem', descricao: 'Realizo a limpeza de quintais', valor: '150.00', horarioInicio: '2000-01-01 07:30:00 UTC',
                           horarioTermino: '2000-01-01 08:30:00 UTC', data: '2022-12-30', trabalhador_id: trabalhador.id)
     assert servico.save
-    cliente = Cliente.new(nome: 'Teste', cpf: '70369935454', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
+    cliente = Cliente.new(nome: 'Teste', email: 'cliente@admin.com', cpf: '70369935454', cep: '11000-123', cidade: 'Sao Paulo',
+                          bairro: 'Alphaville', logradouro: 'Rua 15', complemento: 'casa', password:'123456', password_confirmation:'123456')
     assert cliente.save
     contrato = Contrato.new(servico_id: servico.id, cliente_id: cliente.id)
     assert contrato.save
@@ -30,7 +31,8 @@ class ContratoTest < ActiveSupport::TestCase
   test "contrato sem servico disponivel" do
     trabalhador = Trabalhador.new(nome: 'Teste', cpf: '70369935454', profissao: 'Pedreiro', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
     assert trabalhador.save
-    cliente = Cliente.new(nome: 'Teste', cpf: '70369935454', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
+    cliente = Cliente.new(nome: 'Teste', email: 'cliente@admin.com', cpf: '70369935454', cep: '11000-123', cidade: 'Sao Paulo',
+                          bairro: 'Alphaville', logradouro: 'Rua 15', complemento: 'casa', password:'123456', password_confirmation:'123456')
     assert cliente.save
     contrato = Contrato.new(cliente_id: cliente.id)
     assert_not contrato.save
