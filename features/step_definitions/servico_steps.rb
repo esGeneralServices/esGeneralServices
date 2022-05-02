3b39f8a627c9ab2eb36655e83bde26075403b514
FabioFreire-ff <fabiosbfreire@gmail.com>
2022-05-02 15:31:52 -0300

    Adicionando implementação do Scenario: Cadastrar um novo servico.

---
+++ features/step_definitions/servico_steps.rb
@@ -0,0 +1,17 @@
+Given('Eu estou na tela de cadastrar um novo servico') do
+  visit '/servicos/new'
+end
+
+When('Eu crio um servico com nome {string}, descricao {string}, valor {string}, horarioInicio {string}, horarioTermino {string}, data {string}') do |nome, descricao, valor, horarioInicio, horarioTermino, data|
+  fill_in 'servico[nome]',:with => nome
+  fill_in 'servico[descricao]',:with => descricao
+  fill_in 'servico[valor]',:with => valor
+  fill_in 'servico[horarioInicio]',:with => horarioInicio
+  fill_in 'servico[horarioTermino]',:with => horarioTermino
+  fill_in 'servico[data]',:with => data
+  click_button 'Create Servico'
+end
+
+Then('Eu vejo uma mensagem que o serviço de nome {string} foi criado') do |nome|
+  expect(page).to have_content(nome)
+end
