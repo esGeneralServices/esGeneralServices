2a8a53c10e3ba9b318e993e3e1760a8040785635
jurasmelo <jurandir.melo@ufrpe.br>
2022-05-02 00:35:26 -0300

    Alteração da view clientes/edit para a apresentação dos campos de Endereço para a atualização

--- app/controllers/clientes/registrations_controller.rb
+++ app/controllers/clientes/registrations_controller.rb
@@ -42,12 +42,12 @@ class Clientes::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome , :cpf])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome , :cpf, :cep, :cidade, :bairro, :logradouro, :complemento])
    end
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
-     devise_parameter_sanitizer.permit(:account_update, keys: [:nome, :cpf])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:nome, :cpf, :cep, :cidade, :bairro, :logradouro, :complemento])
    end
 
   # The path used after sign up.
--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -11,10 +11,10 @@ class Cliente < ApplicationRecord
   validate :cpf_valida
 
   validates_format_of :cep, presence: true, with: /\A\d{5}-\d{3}\z/, message: "Cep no formato: 0000-000"
-  validates :cidade, presence: true, length: {in: 3..30}, format: {with: /\A[a-zA-z ]+\z/, message: "Apenas Letras"}
-  validates :bairro, presence: true, length: {in: 3..30}, format: {with: /\A[a-zA-z ]+\z/, message: "Apenas Letras"}
-  validates :logradouro, presence: true, length: {in: 3..30}, format: {with: /\A[a-zA-z0-9 ]+\z/, message: "Apenas Letras e números"}
-  validates :complemento, presence: true, length: {in: 0..30}, format: {with: /\A[a-zA-z0-9 ]+\z/, message: "Apenas Letras e números"}
+  validates :cidade, presence: true, length: {in: 3..30}, format: {with: /\A[a-zA-z ]+\z/, message: "Utilize apenas letras"}
+  validates :bairro, presence: true, length: {in: 3..30}, format: {with: /\A[a-zA-z ]+\z/, message: "Utilize apenas letras"}
+  validates :logradouro, presence: true, length: {in: 3..30}, format: {with: /\A[a-zA-z0-9 ]+\z/, message: "Utilize apenas letras e números"}
+  validates :complemento, presence: true, length: {in: 0..30}, format: {with: /\A[a-zA-z0-9 ]+\z/, message: "Utilize apenas letras e números"}
 
   def cpf_valida
     if cpf.present? && !CPF.valid?(cpf)
