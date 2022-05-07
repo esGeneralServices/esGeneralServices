42acddf740b19514d268dd3b2f7da0c0a7e65bb1
jurasmelo <jurandir.melo@ufrpe.br>
2022-05-06 22:41:57 -0300

    Refatorando o test do Model Cliente - Cliente Válido

--- test/models/cliente_test.rb
+++ test/models/cliente_test.rb
@@ -7,16 +7,16 @@ class ClienteTest < ActiveSupport::TestCase
                            bairro: 'Alphaville', logradouro: 'Rua 15', complemento: 'casa', password:'123456', password_confirmation:'123456')
   end
 
+  test 'cliente valido' do
+    assert @cliente.valid?
+  end
+
   test 'cliente sem nome' do
     cliente = Cliente.new(nome: '', email: 'cliente@admin.com', cpf: '70369935454', cep: '11000-123', cidade: 'Sao Paulo',
                           bairro: 'Alphaville', logradouro: 'Rua 15', complemento: 'casa', password:'123456', password_confirmation:'123456')
     assert_not cliente.save
   end
-  test 'cliente valido' do
-    cliente = Cliente.new(nome: 'Teste', email: 'cliente@admin.com', cpf: '70369935454', cep: '11000-123', cidade: 'Sao Paulo',
-                          bairro: 'Alphaville', logradouro: 'Rua 15', complemento: 'casa', password:'123456', password_confirmation:'123456')
-    assert cliente.save
-  end
+
   test 'cliente sem email' do
     cliente = Cliente.new(nome: 'Teste', email: '', cpf: '70369935454', cep: '11000-123', cidade: 'Sao Paulo',
                           bairro: 'Alphaville', logradouro: 'Rua 15', complemento: 'casa', password:'123456', password_confirmation:'123456')
