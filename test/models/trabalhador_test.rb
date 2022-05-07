c141862ca58c679875b2f8bbddd298169942f2d4
jurasmelo <jurandir.melo@ufrpe.br>
2022-05-06 22:55:09 -0300

    Refatorando o test do Model Trabalhador - Trabalhador Válido

--- test/models/trabalhador_test.rb
+++ test/models/trabalhador_test.rb
@@ -3,18 +3,19 @@ require "test_helper"
 class TrabalhadorTest < ActiveSupport::TestCase
 
   def setup
-    @trabalhador = Trabalhador.new(nome: '', cpf: '70369935454', profissao: 'Pedreiro',
+    @trabalhador = Trabalhador.new(nome: 'José da Silva', cpf: '70369935454', profissao: 'Pedreiro',
                                    email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
+  end
+
+  test 'trabalhador valido' do
+    assert @trabalhador.valid?
   end
 
   test 'trabalhador sem nome' do
     trabalhador = Trabalhador.new(nome: '', cpf: '70369935454', profissao: 'Pedreiro', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
     assert_not trabalhador.save
   end
-  test 'trabalhador valido' do
-    trabalhador = Trabalhador.new(nome: 'Teste', cpf: '70369935454', profissao: 'Pedreiro', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
-    assert trabalhador.save
-  end
+
   test 'trabalhador sem profissao' do
     trabalhador = Trabalhador.new(nome: 'Teste', cpf: '70369935454', profissao: '', email: 'trabalhador@email.com', password:'123456', password_confirmation:'123456')
     assert_not trabalhador.save
