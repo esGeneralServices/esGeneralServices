c6ed8e8bf52fc9b4c0f402c06de88b183f4f1ea8
jurasmelo <jurandir.melo@ufrpe.br>
2022-05-06 22:39:19 -0300

    Refatorando o test do Model Cliente - Criando Cliente padrão

--- test/models/cliente_test.rb
+++ test/models/cliente_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class ClienteTest < ActiveSupport::TestCase
+
+  def setup
+    @cliente = Cliente.new(nome: 'Antonio Carlos', email: 'cliente@admin.com', cpf: '70369935454', cep: '11000-123', cidade: 'Sao Paulo',
+                           bairro: 'Alphaville', logradouro: 'Rua 15', complemento: 'casa', password:'123456', password_confirmation:'123456')
+  end
+
   test 'cliente sem nome' do
     cliente = Cliente.new(nome: '', email: 'cliente@admin.com', cpf: '70369935454', cep: '11000-123', cidade: 'Sao Paulo',
                           bairro: 'Alphaville', logradouro: 'Rua 15', complemento: 'casa', password:'123456', password_confirmation:'123456')
